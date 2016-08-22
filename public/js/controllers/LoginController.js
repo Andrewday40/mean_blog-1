62821657269ba5150d9b8dfe3542ba7e3a34a6e4
Justin Dennison <justin1dennison@gmail.com>
2016-08-22 19:39:32 -0400

    finished comments for the login controller

--- public/js/controllers/LoginController.js
+++ public/js/controllers/LoginController.js
@@ -2,20 +2,37 @@
   angular.module('ngBlog')
           .controller('LoginController', LoginController);
 
-  LoginController.$inject = ['$scope', 'AuthService', '$location', '$route'];
+  LoginController.$inject = ['$scope', 'AuthService', '$location'];
 
-  function LoginController($scope, AuthService, $location, $route){
-    $scope.login = login;
 
-    $scope.user = {
-      email: 'justin@dennison.com',
-      password: 'password'
-    };
+  /**
+   * LoginController - description
+   *
+   * @param  {object} $scope      Angular Scope object for using data and functions
+   *                              within the DOM
+   * @param  {object} AuthService Our custom written AuthService that allows for
+   *                            Authentication and persistence of a user
+   * @param  {object} $location   Angular service that will allow the redirect to a url
+   * @return {none}             None
+   */
+  function LoginController($scope, AuthService, $location){
+    $scope.login = login; //exposing the login function to the scope
+
+
+    /**
+     * login - This is a wrapper to the login function of the AuthService
+     *          that will login a user based on username and password then
+     *          redirect to the home page when that is successful. Currently,
+     *          this does not handle the error case... Could you implement that?
+     *
+     * @param  {object} user This is the user object that contains an email and password
+     *                    that is used to login to the backend.
+     * @return {None}      None
+     */
     function login(user){
       AuthService.login(user)
             .then(function(){
               $location.path('/');
-              // $route.reload();
             })
             .catch(function(err){
               console.log(err);
