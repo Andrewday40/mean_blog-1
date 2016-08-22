b359ff8df25a8bd52ff976759744331472b8cecd
Justin Dennison <justin1dennison@gmail.com>
2016-08-22 19:35:03 -0400

    added comments for the getPosts function in home controller

--- public/js/controllers/HomeController.js
+++ public/js/controllers/HomeController.js
@@ -10,6 +10,16 @@
 
     getPosts();
 
+
+    /**
+     * getPosts - Sets the posts on the $scope for the landing page.
+     *
+     * @return {type}  Nothing is returned. This utilizes scope to expose a series
+     *                of posts for the landing page of the blog. At current, it is
+     *                getting 5 random posts from the database, but could be
+     *                changed to the 5 most recent posts by changing the PostService
+     *                function call.
+     */
     function getPosts(){
       PostService.getRandom(5)
               .then(function(response){
