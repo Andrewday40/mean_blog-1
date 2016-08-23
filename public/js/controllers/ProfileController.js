bfca9ccf65b58ee47c2d71170ef287a9595b20b3
Justin Dennison <justin1dennison@gmail.com>
2016-08-22 20:55:29 -0400

    completed profile controller comments

--- public/js/controllers/ProfileController.js
+++ public/js/controllers/ProfileController.js
@@ -5,9 +5,10 @@
   ProfileController.$inject = ['$scope', 'PostService', 'AuthService'];
 
   function ProfileController($scope, PostService, AuthService){
-    $scope.posts = [];
+    $scope.posts = []; //posts data
     $scope.deletePost = deletePost;
-    $scope.user = AuthService.currentUser();
+    $scope.user = AuthService.currentUser(); //link the current user to the user
+                                            // user must be linked in
 
     getPosts();
 
@@ -21,6 +22,15 @@
                 });
     }
 
+
+    /**
+     * deletePost - deletes the post from the front end without requesting an update
+     *              the splice from the DOM for performance
+     *
+     * @param  {object} post  post object
+     * @param  {number} index position of the post in the collection
+     * @return {none}       none
+     */
     function deletePost(post, index){
       PostService.deletePost(post._id)
               .then(function(response){
